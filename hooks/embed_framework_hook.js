5e149a0a4f39ab13ab4b387bdb233290d99aae69
edaviok1301 <eparedesba1301@gmail.com>
2021-05-18 15:21:54 -0500

    add try catch all hook

--- hooks/embed_framework_hook.js
+++ hooks/embed_framework_hook.js
@@ -16,116 +16,118 @@ const xcode = require('xcode'),
     path = require('path');
 
 module.exports = function (ctx) {
+    try{
+        // IMPORTANT!!
+        // Replace the following var with the correct name of the .framework file to be embed
+        var frameworkName = "Payme.framework";
 
-    // IMPORTANT!!
-    // Replace the following var with the correct name of the .framework file to be embed
-    var frameworkName = "Payme.framework";
+        /*var fs = ctx.requireCordovaModule("fs");
+        var path = ctx.requireCordovaModule("path");
+        var xcode = ctx.requireCordovaModule("xcode");
+        var deferral = ctx.requireCordovaModule('q').defer();*/
 
-    /*var fs = ctx.requireCordovaModule("fs");
-    var path = ctx.requireCordovaModule("path");
-    var xcode = ctx.requireCordovaModule("xcode");
-    var deferral = ctx.requireCordovaModule('q').defer();*/
-
-    /**
-     * Recursively search for file with the tiven filter starting on startPath
-     */
-    function searchRecursiveFromPath(startPath, filter, rec, multiple) {
-        if (!fs.existsSync(startPath)) {
-            console.log("no dir ", startPath);
-            return;
-        }
-
-        var files = fs.readdirSync(startPath);
-        var resultFiles = []
-        for (var i = 0; i < files.length; i++) {
-            var filename = path.join(startPath, files[i]);
-            var stat = fs.lstatSync(filename);
-            if (stat.isDirectory() && rec) {
-                fromDir(filename, filter); //recurse
+        /**
+         * Recursively search for file with the tiven filter starting on startPath
+         */
+        function searchRecursiveFromPath(startPath, filter, rec, multiple) {
+            if (!fs.existsSync(startPath)) {
+                console.log("no dir ", startPath);
+                return;
             }
 
-            if (filename.indexOf(filter) >= 0) {
-                if (multiple) {
-                    resultFiles.push(filename);
-                } else {
-                    return filename;
+            var files = fs.readdirSync(startPath);
+            var resultFiles = []
+            for (var i = 0; i < files.length; i++) {
+                var filename = path.join(startPath, files[i]);
+                var stat = fs.lstatSync(filename);
+                if (stat.isDirectory() && rec) {
+                    fromDir(filename, filter); //recurse
+                }
+
+                if (filename.indexOf(filter) >= 0) {
+                    if (multiple) {
+                        resultFiles.push(filename);
+                    } else {
+                        return filename;
+                    }
+                }
+            }
+            if (multiple) {
+                return resultFiles;
+            }
+        }
+
+        /**
+         * find a PBXFileReference on the provided project by its name
+         */
+        function findPbxFileReference(project, pbxFileName) {
+            for (var uuid in project.hash.project.objects.PBXFileReference) {
+                if (uuid.endsWith("_comment")) {
+                    continue;
+                }
+                var file = project.hash.project.objects.PBXFileReference[uuid];
+
+                if (file.name !== undefined && file.name.indexOf(pbxFileName) != -1) {
+                    return file;
                 }
             }
         }
-        if (multiple) {
-            return resultFiles;
-        }
-    }
 
-    /**
-     * find a PBXFileReference on the provided project by its name
-     */
-    function findPbxFileReference(project, pbxFileName) {
-        for (var uuid in project.hash.project.objects.PBXFileReference) {
-            if (uuid.endsWith("_comment")) {
-                continue;
-            }
-            var file = project.hash.project.objects.PBXFileReference[uuid];
-
-            if (file.name !== undefined && file.name.indexOf(pbxFileName) != -1) {
-                return file;
+        if (process.length >= 5 && process.argv[1].indexOf('cordova') == -1) {
+            if (process.argv[4] != 'ios') {
+                return; // plugin only meant to work for ios platform.
             }
         }
-    }
 
-    if (process.length >= 5 && process.argv[1].indexOf('cordova') == -1) {
-        if (process.argv[4] != 'ios') {
-            return; // plugin only meant to work for ios platform.
+        var xcodeProjPath = searchRecursiveFromPath('platforms/ios', '.xcodeproj', false);
+        var projectPath = xcodeProjPath + '/project.pbxproj';
+        console.log("Found", projectPath);
+
+        var proj = xcode.project(projectPath);
+        proj.parseSync();
+
+
+
+        var frameworkPbxFileRef = findPbxFileReference(proj, frameworkName);
+        // Clean extra " on the start and end of the string
+        var frameworkPbxFileRefPath = frameworkPbxFileRef.path;
+        console.log("frameworkPbxFileRefPath",frameworkPbxFileRefPath);
+        if (frameworkPbxFileRefPath.endsWith("\"")) {
+            frameworkPbxFileRefPath = frameworkPbxFileRefPath.substring(0, frameworkPbxFileRefPath.length - 1);
         }
-    }
-
-    var xcodeProjPath = searchRecursiveFromPath('platforms/ios', '.xcodeproj', false);
-    var projectPath = xcodeProjPath + '/project.pbxproj';
-    console.log("Found", projectPath);
-
-    var proj = xcode.project(projectPath);
-    proj.parseSync();
-
-
-
-    var frameworkPbxFileRef = findPbxFileReference(proj, frameworkName);
-    // Clean extra " on the start and end of the string
-    var frameworkPbxFileRefPath = frameworkPbxFileRef.path;
-    console.log("frameworkPbxFileRefPath",frameworkPbxFileRefPath);
-    if (frameworkPbxFileRefPath.endsWith("\"")) {
-        frameworkPbxFileRefPath = frameworkPbxFileRefPath.substring(0, frameworkPbxFileRefPath.length - 1);
-    }
-    if (frameworkPbxFileRefPath.startsWith("\"")) {
-        frameworkPbxFileRefPath = frameworkPbxFileRefPath.substring(1, frameworkPbxFileRefPath.length);
-    }
-
-    // If the build phase doesn't exist, add it
-    if (proj.pbxEmbedFrameworksBuildPhaseObj(proj.getFirstTarget().uuid) == undefined) {
-        console.log("BuildPhase not found in XCode project. Adding PBXCopyFilesBuildPhase - Embed Frameworks");
-        proj.addBuildPhase([], 'PBXCopyFilesBuildPhase', "Embed Frameworks", proj.getFirstTarget().uuid, 'frameworks');
-    }
-
-    // Now remove the framework
-    var removedPbxFile = proj.removeFramework(frameworkPbxFileRefPath, {
-        customFramework: true
-    });
-    // Re-add the framework but with embed
-    var addedPbxFile = proj.addFramework(frameworkPbxFileRefPath, {
-        customFramework: true,
-        embed: true,
-        sign: true
-    });
-
-
-    fs.writeFile(proj.filepath, proj.writeSync(), 'utf8', function (err) {
-        if (err) {
-            console.log("finished writing xcodeproj whit error");
-            //deferral.reject(err);
-            return;
+        if (frameworkPbxFileRefPath.startsWith("\"")) {
+            frameworkPbxFileRefPath = frameworkPbxFileRefPath.substring(1, frameworkPbxFileRefPath.length);
         }
-        console.log("finished writing xcodeproj");
-        //deferral.resolve();
-    });
 
-    //return deferral.promise;
+        // If the build phase doesn't exist, add it
+        if (proj.pbxEmbedFrameworksBuildPhaseObj(proj.getFirstTarget().uuid) == undefined) {
+            console.log("BuildPhase not found in XCode project. Adding PBXCopyFilesBuildPhase - Embed Frameworks");
+            proj.addBuildPhase([], 'PBXCopyFilesBuildPhase', "Embed Frameworks", proj.getFirstTarget().uuid, 'frameworks');
+        }
+
+        // Now remove the framework
+        var removedPbxFile = proj.removeFramework(frameworkPbxFileRefPath, {
+            customFramework: true
+        });
+        // Re-add the framework but with embed
+        var addedPbxFile = proj.addFramework(frameworkPbxFileRefPath, {
+            customFramework: true,
+            embed: true,
+            sign: true
+        });
+
+
+        fs.writeFile(proj.filepath, proj.writeSync(), 'utf8', function (err) {
+            if (err) {
+                console.log("finished writing xcodeproj whit error");
+                //deferral.reject(err);
+                return;
+            }
+            console.log("finished writing xcodeproj");
+            //deferral.resolve();
+        });    
+        //return deferral.promise;
+    }catch(err){
+        console.log(err)
+    }
 };
